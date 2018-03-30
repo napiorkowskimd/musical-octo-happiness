721894be76a7c2abadd4f1cf56c65b2fa1cf5e48
Michał Napiórkowski <napiorkowskimd@gmail.com>
2018-03-30 10:22:53 +0200

    main.cpp z gałęzi Pulsowanie

--- main.cpp
+++ main.cpp
@@ -1,27 +1,66 @@
+
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <string>
-
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {
-	String imageName("resources/kot.jpeg"); // by default
+	String imageName("resources/happy-fish.jpg"); // by default
 	if (argc > 1)
 	{
 		imageName = argv[1];
 	}
 	Mat image;
 	image = imread(imageName, IMREAD_COLOR); // Read the file
-	if (image.empty())                      // Check for invalid input
+	if (image.empty()) // Check for invalid input
 	{
-		cout << "Could not open or find the image, :(" << std::endl;
+		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image);                // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
+
+	Mat image_hsv;
+	Mat image_tmp;
+	cvtColor(image, image_hsv, COLOR_BGR2HSV);
+/*	for (int i = 0; i < image_hsv.rows; i++)
+	{
+		for (int j = 0; j < image_hsv.cols; j++)
+		{
+			image_hsv.at<Vec3b>(i, j)[2] = saturate_cast<uchar>(image_hsv.at<Vec3b>(i, j)[2] * 2);
+		}
+	}*/
+
+	namedWindow("... ", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("... ", image); // Show our image inside it.
+
+
+	
+	
+	int k = 0;
+	int l = 1;
+	namedWindow("Zmienione ", WINDOW_AUTOSIZE); // Create a window for display.
+	
+	while (waitKey(1) == -1) {
+//
+//		for (int i = 0; i < image_hsv.rows; i++)
+//		{
+//			for (int j = 0; j < image_hsv.cols; j++)
+//			{
+//				image_hsv.at<Vec3b>(i, j)[2] = saturate_cast<uchar>(image_hsv.at<Vec3b>(i, j)[2] +2);
+//			}
+//	}
+		if (k == 10)
+		{
+			l = -l;
+		}
+		image_hsv = image_hsv + Scalar(0, 0, 5*l);
+		k = (k+1) % 11;
+		cvtColor(image_hsv, image, COLOR_HSV2BGR);
+		imshow("Zmienione ", image);
+	} // Wait for a keystroke in the window
 	return 0;
 }
